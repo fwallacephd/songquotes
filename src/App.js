45f57f4f06b4ec8b3ccddaa64cf0eb83b68def19
Faith Wallace <fwallacephd@gmail.com>
2018-09-21 14:18:04 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="row">
+          <div className="col-md-12 text-center">
+            <p className="title text-center">Random Quote Generator</p>
+            <p className="subtitle text-center"><i class="fas fa-headphones-alt"></i>  Music Edition  <i className="fas fa-headphones-alt"></i></p>
+          </div>
+        </div>
+      </div>
+
+    );
+  }
+}
+
+export default App;
---
+++ src/Song/Song.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const song = (props) => {
+  return (
+    <div className="row backgroundImage">
+      <div className="col-md-12 songInformation">
+      </div>
+    </div>
+    
+  );
+}
+
+export default song;
